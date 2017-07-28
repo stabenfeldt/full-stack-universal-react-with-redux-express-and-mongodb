a2c418add1a2073a80d0c02646d1eeeaf93b48ca
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-07-28 15:08:49 +0200

    fix

--- src/components/pages/bookItem.js
+++ src/components/pages/bookItem.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import {Row, Col, Well, Button} from 'react-bootstrap'
+import {Row, Col, Well, Button} from 'react-bootstrap';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {AddToCart} from '../../../actions/cartActions'
+
+
 
 class BookItem extends React.Component{
   render() {
@@ -10,7 +15,7 @@ class BookItem extends React.Component{
             <h6> {this.props.title} </h6>
             <p> {this.props.description} </p>
             <h6> usd: {this.props.price} </h6>
-            <Button bsStyle='primary'>But now </Button>
+            <Button bsStyle='primary'>Buy now </Button>
           </Col>
         </Row>
       </Well>
